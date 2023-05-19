602ab979232678a1280419f41943b715881d675c
SeooyeonJ <130852344+SeooyeonJ@users.noreply.github.com>
2023-05-19 15:18:40 +0900

    Commit Message.

--- app/src/main/java/com/example/woodonggo/Home_Fragment_Main.java
+++ app/src/main/java/com/example/woodonggo/Home_Fragment_Main.java
@@ -1,19 +1,42 @@
 package com.example.woodonggo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class Home_Fragment_Main extends Fragment {
+
+    FloatingActionButton floatingBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.home_fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        floatingBtn = view.findViewById(R.id.floatingBtn);
+        floatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(Home_Fragment_Main.this, Post_MainActivity.class);
+                //startActivity(intent);
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
     }
 }
