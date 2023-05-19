6dea56bfa72013391fe329fff7878930353169ab
minkyeongcho <jjoming129@naver.com>
2023-05-19 16:26:09 +0900

    commit

--- app/src/main/java/com/example/woodonggo/Post_MainActivity.java
+++ app/src/main/java/com/example/woodonggo/Post_MainActivity.java
@@ -10,6 +10,6 @@ public class Post_MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.post_activity_main);
+        setContentView(R.layout.home_post);
     }
 }
