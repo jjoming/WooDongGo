81bca202cfd10496ccdcfa017b567f85104601cc
SeooyeonJ <130852344+SeooyeonJ@users.noreply.github.com>
2023-05-27 05:29:09 +0900

    Commit Message.

--- app/src/main/java/com/example/woodonggo/Home_Fragment_Main.java
+++ app/src/main/java/com/example/woodonggo/Home_Fragment_Main.java
@@ -15,16 +15,36 @@ import android.widget.FrameLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class Home_Fragment_Main extends Fragment {
-
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
     FloatingActionButton floatingBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment_main, container, false);
+        View view = inflater.inflate(R.layout.home_fragment_main, container, false);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        Home_ViewPager_Adapter home_viewPager_adapter = new Home_ViewPager_Adapter(requireActivity());
+        viewPager.setAdapter(home_viewPager_adapter);
+
+        // TabLayout과 ViewPager 연동
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("팀");
+                            break;
+                        case 1:
+                            tab.setText("개인");
+                            break;
+                    }
+                }).attach();
+
+        return view;
     }
 
     @Override
@@ -33,8 +53,7 @@ public class Home_Fragment_Main extends Fragment {
         floatingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(Home_Fragment_Main.this, Post_MainActivity.class);
-                //startActivity(intent);
+                //TODO:게시글 작성 화면으로 이동
             }
         });
         super.onViewCreated(view, savedInstanceState);
