ee40b948a6a9b3fb98cdc4348b61198c1d4e7e86
jjihong <128901284+jjihong@users.noreply.github.com>
2023-05-27 05:42:13 +0900

    commit

---
+++ app/src/main/java/com/example/woodonggo/Login_Main_Activity.java
@@ -0,0 +1,15 @@
+package com.example.woodonggo;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_Main_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+
+    }
+}
